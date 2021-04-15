7c28cd4358ddc585d8f50133c29045c6695d6d4c
sangwoo310 <sangwoo310@btckorea.com>
2021-04-15 15:14:17 +0900

    commit

--- src/main/java/com/hsw/algorithm/AlgorithmApplication.java
+++ src/main/java/com/hsw/algorithm/AlgorithmApplication.java
@@ -8,6 +8,7 @@ public class AlgorithmApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AlgorithmApplication.class, args);
+        System.out.println("system is run");
     }
 
 }
