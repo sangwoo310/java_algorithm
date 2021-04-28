afc17ff0c53e1f4280f58183ede436e6676daf41
sangwoo310 <sangwoo310@btckorea.com>
2021-04-28 15:43:03 +0900

    백준 단계 문자열 완료

--- src/main/java/com/hsw/algorithm/baekjoon/characters/CharacterCount_1152.java
+++ src/main/java/com/hsw/algorithm/baekjoon/characters/CharacterCount_1152.java
@@ -1,4 +1,16 @@
 package com.hsw.algorithm.baekjoon.characters;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class CharacterCount_1152 {
+    public static void main(String []args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
+
+        System.out.println(st.countTokens());
+    }
 }
--- src/main/java/com/hsw/algorithm/baekjoon/characters/Dial_5622.java
+++ src/main/java/com/hsw/algorithm/baekjoon/characters/Dial_5622.java
@@ -1,4 +1,25 @@
 package com.hsw.algorithm.baekjoon.characters;
 
-public class baka_5622 {
+import java.io.IOException;
+
+public class Dial_5622 {
+    public static void main(String []args) throws IOException {
+        int time = 0;
+        int count = 0;
+
+        int []alphabetArr = {2,2,2,3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,7,8,8,8,9,9,9,9};
+
+        while (true) {
+            int input = System.in.read();
+            if (input == 10) {
+                break;
+            }
+
+            time += alphabetArr[input - 65];
+            count++;
+        }
+
+        System.out.println(time + count);
+
+    }
 }
