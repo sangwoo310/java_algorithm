15d57bdd1cc7fec50da9eb1c12a077733dd68477
sangwoo <sangwoo310@naver.com>
2023-07-04 21:10:23 +0900

    BiggerRight - use stack and seq. It need to try agagin for remember to use this way

--- src/main/java/com/hsw/algorithm/baekjoon/basicAlgorithm1/dataStructure2/BiggerRight_17298.java
+++ src/main/java/com/hsw/algorithm/baekjoon/basicAlgorithm1/dataStructure2/BiggerRight_17298.java
@@ -1,43 +1,49 @@
 package com.hsw.algorithm.baekjoon.basicAlgorithm1.dataStructure2;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class BiggerRight_17298 {
-
-  // Q: 아래 코드의 시간복잡도가 얼마야?
-  // A: O(N^2)
-  // Q: 이유는?
-  // A: N개의 수를 입력받고, N개의 수를 비교하기 때문에 N^2이다.
+  /**
+   * 이 문제는 seq 와 stack 을 적절히 사용하는건데 이해하기가 어려움...
+   * seq 를 stack 에 저장하여 기록을 남기고 stack 에 저장된값 이전에는 전부다 큰값으로 바꾸면서 처리
+   * stack 에 저장된 seq 를 큰값으로 바꾸게 되면 자동으로 정렬이 된다
+   *
+   * 참고자료: https://st-lab.tistory.com/196
+   */
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
     int N = Integer.parseInt(br.readLine());
 
     StringTokenizer st = new StringTokenizer(br.readLine());
-    Deque<Integer> deque = new ArrayDeque<>();
-    while (st.hasMoreTokens()) {
-      deque.add(Integer.parseInt(st.nextToken()));
+    int[] seq = new int[N];
+    for(int i=0; i<N; i++) {
+      seq[i] = Integer.parseInt(st.nextToken());
     }
 
-    int largestNum = -1;
-    for (int i=N; i>0; i--) {
-      int baseNum = deque.getFirst();
-      largestNum = -1;
+    Stack<Integer> stack = new Stack<>();
 
-      for(int j=0; j<i-1; j++) {
-        int compareNum = deque.pollLast();
-        if(baseNum < compareNum) {
-          largestNum = compareNum;
-        }
-        deque.addFirst(compareNum);
+    for(int i=0; i<N; i++) {
+      while (!stack.isEmpty() && seq[stack.peek()] < seq[i]) {
+        seq[stack.pop()] = seq[i];
       }
-      bw.write(largestNum + " ");
-      deque.pollLast();
+
+      stack.push(i);
     }
 
-    bw.flush();
-    bw.close();
-    br.close();
+    while (!stack.isEmpty()) {
+      seq[stack.pop()] = -1;
+    }
+
+    StringBuilder sb = new StringBuilder();
+    for(int i = 0; i < N; i++) {
+      sb.append(seq[i]).append(' ');
+    }
+
+    System.out.println(sb);
   }
 }
