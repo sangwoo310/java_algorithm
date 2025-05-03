ac5393a98159b72f7eaf4b23e714486ddffa6f67
sangwoo <sangwoo310@github.com>
2025-05-03 20:09:05 +0900

    dp 를 사자의 마리수가 아니라 왼쪽 오른쪽 존재안할때로 나누어 계산 -> 조건을 자세히 보고 조건을 간단하게 제한하여 생각해야함

--- src/main/java/com/hsw/algorithm/dp/Zoo_1309.java
+++ src/main/java/com/hsw/algorithm/dp/Zoo_1309.java
@@ -3,6 +3,7 @@ package com.hsw.algorithm.dp;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Zoo_1309 {
   public static void main(String[] args) throws IOException {
@@ -11,22 +12,38 @@ public class Zoo_1309 {
 
     /**
      *
-     * dp[0] = 1, dp[1] = 1 + 2 = 3, dp[2] = 1 + 4 + 2 = 7, dp[3] = 1 + 6 + 12 + 2 = 21, dp[4] = 1 + 8 + 18 + 10 + 2 = 41
-     * 12 + 2 +
+     * N[1] = 3, N[2] = 7, N[3] = 17, N[4] = 41
+     * dp 를 이차배열로 하여 0 일때는 없을떄 1은 왼쪽 2는 오른쪽에 배치를 선택
+     * dp[1][0] = 1
+     * dp[1][1] = 1
+     * dp[1][2] = 1
+     * dp[2][0] = 3
+     * dp[2][1] = 2
+     * dp[2][2] = 2
+     * dp[3][0] = 7
+     * dp[3][1] = 5
+     * dp[3][2] = 5
      *
+     * dp[N][0] = dp[N-1][0] + dp[N-1][1] + dp[N-1][2]
+     * dp[N][1] = dp[N-1][0] + dp[N-1][2]
+     * dp[N][2] = dp[N-1][0] + dp[N-1][1]
      *
      */
-    int[][] dp = new int[3][N+1];
 
-    for (int i=2; i<=N; i++) {
-      dp[0][i] = (dp[0][i-1] + dp[1][i-1] + dp[2][i-1]) % 9901;
-      dp[1][i] = (dp[2][i-1] + dp[0][i-1]) % 9901;
-      dp[2][i] = (dp[1][i-1] + dp[0][i-1]) % 9901;
+    int[][] dp = new int[N + 1][3];
+    Arrays.fill(dp[1], 1);
+
+    for (int i = 2; i < N + 1; i++) {
+      dp[i][0] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]) % 9901;
+      dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % 9901;
+      dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) % 9901;
     }
-    int sum = 0;
-    for(int i =0; i<3; i++){
-      sum+=dp[i][N];
+
+    int result = 0;
+    for (int i = 0; i < 3; i++) {
+      result += dp[N][i];
     }
-    System.out.println(sum%9901);
+
+    System.out.println(result % 9901);
   }
 }
